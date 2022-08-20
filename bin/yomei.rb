f187e57c23c38649a4d49958bffe62e3e7525e64
Taylor Collins <taylorcc33@gmail.com>
2022-08-20 17:14:35 -0600

    work again

--- bin/yomei.rb
+++ bin/yomei.rb
@@ -1,9 +1,24 @@
 #!/usr/bin/env ruby
+require 'date'
+require 'pry'
 
-birthday = Time.new(1986, 12, 26)
-death_date = Time.new(2062, 12, 26)
 
-difference = death_date - birthday
-difference = difference / 52
+date1 = Date.new(1986, 12, 26)
+date2 = Date.new(2062, 12, 26)
 
-p difference
+date_range = (date1..date2).select { |d| d.day == 1 }
+
+binding.pry
+
+p date_range.to_a
+
+
+
+
+
+
+
+
+# Iterate over total months.
+#   - print [x] if current month is before Time.now
+#   - print [ ] if current month is after Time.now
