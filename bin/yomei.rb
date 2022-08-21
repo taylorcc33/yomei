3af8361e408bcefb3c21c39dc2215ebae497c991
Taylor Collins <taylorcc33@gmail.com>
2022-08-20 18:24:31 -0600

    prints boxes, and finished

--- bin/yomei.rb
+++ bin/yomei.rb
@@ -2,23 +2,17 @@
 require 'date'
 require 'pry'
 
-
 date1 = Date.new(1986, 12, 26)
 date2 = Date.new(2062, 12, 26)
 
-date_range = (date1..date2).select { |d| d.day == 1 }
+date_range = ((date1..date2).select { |d| d.day == 1 }).to_a
 
-binding.pry
-
-p date_range.to_a
-
-
-
-
-
-
-
-
-# Iterate over total months.
-#   - print [x] if current month is before Time.now
-#   - print [ ] if current month is after Time.now
+puts "75 years worth of months: "
+date_range.each do |date|
+  current_date = Date.today
+  if date < current_date
+    print "[x]"
+  else
+    print "[ ]"
+  end
+end
